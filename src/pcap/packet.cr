3be4535bd0e16718157fcac07fdb19a911866ac1
Bri in The Sky <brianh6854@googlemail.com>
2016-10-11 16:14:53 +0100

    My assortment of flags not Maiha's

--- src/pcap/packet.cr
+++ src/pcap/packet.cr
@@ -64,12 +64,12 @@ module Pcap
       IpAddr.inspect(ip_header.ip_dst)
     end
     
-    {% for method in %w(ip_v ip_proto ip_hl ip_tos ip_len ip_id ip_ttl ip_sum) %}
+    {% for method in %w(ip_hl ip_id ip_len ip_proto ip_sum ip_tos ip_ttl ip_v) %}
       def {{ method.id }}
         ip_header.{{ method.id }}
       end
     {% end %}
-    {% for method in %w(tcp_src tcp_dst tcp_offx2 tcp_win tcp_sum tcp_urg tcp_seq tcp_ack) %} 
+    {% for method in %w(tcp_data_len tcp_dst tcp_doff tcp_hlen tcp_seq tcp_sum tcp_src tcp_win) %} 
       def {{ method.id }}
         tcp_header.{{ method.id }}
       end
@@ -77,6 +77,7 @@ module Pcap
     # tcp_flags
     {% for key in %w(FIN SYN RST PUSH ACK URG URP ECE CWR) %}
       def tcp_{{key.downcase.id}}?
+        #LibPcap::TH_{{key.id}}
         tcp_header.tcp_{{key.downcase.id}}?
       end
     {% end %}
